d5dbd478dd7c458f22fab6103835790a1c036c0c
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2024-09-15 11:05:37 +0300

    ...

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -897,12 +897,12 @@ exports.acceptPlayInvitation = expressAsyncHandler(async (req, res) => {
 
     const userTokenRtc = await RtcGenerateToken(
       channelName,
-      userId,
+      1,
       "publisher"
     );
     const partnerTokenRtc = await RtcGenerateToken(
       channelName,
-      partnerId,
+      0,
       "publisher"
     );
 
